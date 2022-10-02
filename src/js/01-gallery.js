1d688aaec9803bd42d9f78f15f3cb70410918da5
Serhei Honcharov <mrsedmor@gmail.com>
2022-10-03 00:06:56 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+// В ДЗ використано шаблонізітор pug (для даного налаштування parcel встановити handlebars не вдалось)
+// Попередній код, що використовувався до шаблонізатора, закоментовано
+
+// Шаблон для одного зображення:
+// import createGalleryImageMarkup from '../templates/gallery-image.pug';
+
+// Шаблон для галереї повністю:
+import createGalleryMarkup from '../templates/gallery.pug';
+
+const galleryElem = document.querySelector('.gallery');
+
+// galleryElem.innerHTML = createGalleryMarkup(galleryItems);
+galleryElem.innerHTML = createGalleryMarkup({ images: galleryItems });
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+// function createGalleryMarkup(galleryItems) {
+//   return galleryItems.map(createGalleryImageMarkup).join('');
+// }
+
+// Замість наступної функції використано шаблонізатор pug
+// function createGalleryImageMarkup({ preview, original, description }) {
+//   return `
+//       <a class="gallery__item" href="${original}">
+//         <img class="gallery__image" src="${preview}" alt="${description}" />
+//       </a>`;
+// }
