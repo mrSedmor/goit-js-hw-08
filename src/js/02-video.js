c5fbdb1fd64d8d67cc83712e42e4ebaca0e2fcd6
Serhei Honcharov <mrsedmor@gmail.com>
2022-10-03 21:17:11 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const VIDEOPLAYER_CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const playerElem = document.querySelector('#vimeo-player');
+const player = new VimeoPlayer(playerElem);
+
+restorePlayerTime();
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function restorePlayerTime() {
+  const time = Number(localStorage.getItem(VIDEOPLAYER_CURRENT_TIME_KEY));
+  if (time) {
+    player.setCurrentTime(time);
+  }
+}
+
+function onTimeUpdate(data) {
+  localStorage.setItem(VIDEOPLAYER_CURRENT_TIME_KEY, data.seconds);
+}
